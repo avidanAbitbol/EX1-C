b01d19e7060e79c5677a5d522ff702be87fa5847
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-09 23:12:17 +0200

    Update advancedClassificationRecursion.c

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -2,18 +2,22 @@
 #include <stdio.h>
 #include <NumClass.h>
 
-  int isPalindrome(int num){
-        if(num<0) {return 0;}
-        int temp, digits=0;
-        temp=num;
-        while(temp>0)
-        {
-            temp = temp/10;
-            digits++;
-        }
-
-        return isPalindrome((num/10)%10);
+int isPalindromeParse(int num)
+{
+    int dig = (int)log10(num);
+    if(num == 0)
+        return 0;
+    return ((num%10 * pow(10, digit)) + reverse(num/10));
+}
+int isPalindrome(int num)
+{
+   if(num == reverse(num))
+    {
+        return 1;
     }
+    
+    return 0;
+}
  int isArmstrongParse(int num, int dig){
         if(num<1){return 0; }
         int fact = (pow(num%10,dig));
