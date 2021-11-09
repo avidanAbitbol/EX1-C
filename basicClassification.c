d0f758610d02ef2036b4dde79deacc7c7fc040a4
yukatz <92925727+yukatz@users.noreply.github.com>
2021-11-09 20:57:05 +0200

    Update basicClassification.c

--- basicClassification.c
+++ basicClassification.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 #include <NumClass.h>
-int Factorial(int num)
-{
+int Factorial(int num){
    if(num<=0)
     return 1;
    else
    return num*Factorial(num-1);
 }
- int isStrong(int num){
+int isStrong(int num){
     int fact;
     int sum=0;
-     while(num>0)
+    int newN = num;
+    while(newN>0)
      {
-         fact=1;
-         int rem=num%10;
-         isStrong(num/10);
-         fact=Factorial(rem);
-         sum+=fact;
+         int rem=newN%10;
+         sum+=Factorial(rem);
+         newN=newN/10;
      }
-     return sum;
+     return num==sum;
+   
      }
 int isPrime(int num){
         int ans;
