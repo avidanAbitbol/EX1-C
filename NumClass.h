d6cd09e5e4ec5f2af5532b7d8654fa0d47618c13
avidan.abitbol <avidan88@walla.com>
2021-11-10 14:58:24 +0200

    Update NumClass.h

--- NumClass.h
+++ NumClass.h
@@ -1,8 +1,8 @@
-#ifndef EX_1
-#define EX_1
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#ifndef EX1
+#define EX1
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
 
 int isArmstrong(int x);
 
